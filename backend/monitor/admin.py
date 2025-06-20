9c90f332c7ccfec8d4bae2a474455ca32ecc9ed8
SamMochache <sammochache01@gmail.com>
2025-06-20 22:20:23 +0300

    feat: customize User admin interface to include role information and update display fields

--- backend/monitor/admin.py
+++ backend/monitor/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
+
+admin.site.register(User, CustomUserAdmin)
